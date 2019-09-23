bab8c34c3e905f0f5edb5cc3a4e9c6ac24cbb6dd
stai1 <stai1@scu.edu>
2019-09-23 11:53:59 -0700

    Reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
@@ -26,7 +27,8 @@ import { PostViewComponent } from './post-view/post-view.component';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { delay: 200, dataEncapsulation: false}
-    )
+    ),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post-composer/post-composer.component.ts
+++ src/app/post-composer/post-composer.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { PostService } from '../post.service';
 import { Post } from '../post';
 
 @Component({
   selector: 'app-post-composer',
   templateUrl: './post-composer.component.html',
-  styleUrls: ['./post-composer.component.scss','../post-view/post-view.component.scss']
+  styleUrls: ['./post-composer.component.scss', '../post-view/post-view.component.scss']
 })
 export class PostComposerComponent implements OnInit {
   @Input() posts: Post[];
   editorHidden: boolean = true;
   messageHidden: boolean = true;
+  data: {
+    title: string;
+    content: string;
+    name: string;
+  } = { title: "", content: "", name: "" };
+  form: FormGroup;
   constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(this.data.title),
+      content: new FormControl(this.data.content),
+      name: new FormControl(this.data.name)
+    });
   }
 
   showEditor() {
@@ -24,10 +36,10 @@ export class PostComposerComponent implements OnInit {
     this.editorHidden = true;
     this.messageHidden = true;
   }
-  createPost(title: string, content: string, name: string) {
-    title = title.trim();
-    content = content.trim();
-    name = name.trim();
+  createPost() {
+    var title = this.form.get('title').value.trim();
+    var content = this.form.get('content').value.trim();
+    var name = this.form.get('name').value.trim();
     if (title === "" || content === "" || name === "") {
       this.messageHidden = false;
       return false;
