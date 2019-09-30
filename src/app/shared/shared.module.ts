59f6e9b361a315756ab09ecc83250cd19d5e1825
stai1 <stai1@scu.edu>
2019-09-30 16:09:45 -0700

    Remove import of app-routing.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,7 +14,6 @@ import { RouterModule } from '@angular/router';
     PostComposerComponent
   ],
   imports: [
-    //AppRoutingModule,
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
