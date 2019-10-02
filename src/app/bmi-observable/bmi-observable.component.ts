bffebe15c106dd944b062bbcdda1aeca7d40d71b
stai1 <stai1@scu.edu>
2019-10-02 13:39:09 -0700

    Use timer instead of interval; no initial delay

--- src/app/bmi-observable/bmi-observable.component.ts
+++ src/app/bmi-observable/bmi-observable.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of, interval, combineLatest } from 'rxjs';
+import { of, combineLatest, timer } from 'rxjs';
 import { zip, map } from 'rxjs/operators';
 
 @Component({
@@ -12,8 +12,8 @@ export class BmiObservableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var height = of(1.76, 1.77, 1.78).pipe(zip(interval(1000), a => a));
-    var weight = of(70, 72, 76, 79, 75).pipe(zip(interval(500), a => a));
+    var height = of(1.76, 1.77, 1.78).pipe(zip(timer(0,1000), a => a));
+    var weight = of(70, 72, 76, 79, 75).pipe(zip(timer(0,500), a => a));
     var bmi = combineLatest(weight, height).pipe(map(([w, h]) => { return { w: w, h: h, bmi: w / (h * h) } }));
     bmi.subscribe(result => this.bmiList.push(result));
   }
